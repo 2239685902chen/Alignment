cf053e9e4c07642768a2b7a1a31fb438870c03f0
chen <you@example.com>
2022-05-23 18:55:07 +0800

    添加项目文件。

---
+++ Alignment/对齐.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main()
+{
+	int q = 0;
+	int w = 0;
+	int e = 0;
+	scanf("%d%d%d", &q, &w, &e);
+	printf("%d%8d%8d\n", q, w, e);
+	return 0;
+}
